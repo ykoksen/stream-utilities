d6417de9e1fe3e159d8c0a116d5bf14ceea4d50d
Morten Lindhart <ykoksen@lindhart.com>
2024-06-07 12:53:44 +0200

    Added dispose pattern to buffered background stream

--- Lindhart.Utility.IO.Streaming/Streaming/BufferBackgroundStream.cs
+++ Lindhart.Utility.IO.Streaming/Streaming/BufferBackgroundStream.cs
@@ -54,7 +54,7 @@ namespace Lindhart.Utility.IO.Streaming
             _readBuffer = new MemoryStream(new byte[bufferSize], 0, bufferSize, true, true);
             _writeBuffer = new MemoryStream(new byte[bufferSize], 0, bufferSize, true, true);
             _readBuffer.Position = bufferSize;
-            _writer = Write();
+            _writer = WriteAsync();
         }
 
         public async Task Switch()
@@ -66,10 +66,10 @@ namespace Lindhart.Utility.IO.Streaming
             (_readBuffer, _writeBuffer) = (_writeBuffer, _readBuffer);
 
             // Start writing again in the background
-            _writer = Write();            
+            _writer = WriteAsync();            
         }
 
-        public async Task Write()
+        public async Task WriteAsync()
         { 
             var bytes = _writeBuffer.GetBuffer();
             var bufferSize = bytes.Length;
@@ -83,7 +83,7 @@ namespace Lindhart.Utility.IO.Streaming
                 count = await _innerStream.ReadAsync(bytes, readCount, bufferSize - readCount, _cancellationTokenSource.Token);
                 readCount += count;
             }
-            while (readCount < bufferSize && count != 0);
+            while (readCount < bufferSize && count != 0 && !_cancellationTokenSource.Token.IsCancellationRequested);
 
             _writeBuffer.SetLength(readCount);
             _writeBuffer.Position = 0;            
@@ -97,7 +97,7 @@ namespace Lindhart.Utility.IO.Streaming
         {
             // On purpose the async logic has not been rewritten as sync logic (in order to avoid duplicate code).
             // This is not pretty, but then again why would you use background thread and not run it async?
-            return ReadAsync(new Memory<byte>(buffer, offset, count), CancellationToken.None).Result;
+            return ReadAsync(new Memory<byte>(buffer, offset, count), CancellationToken.None).GetAwaiter().GetResult();
         }
 
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
@@ -130,6 +130,69 @@ namespace Lindhart.Utility.IO.Streaming
         public override void Write(byte[] buffer, int offset, int count)
         {
             throw new System.NotSupportedException();
+        }
+
+        private bool _disposed = false;
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_disposed) return;
+
+            if (disposing)
+            {
+                _cancellationTokenSource.Cancel();
+
+                try
+                {
+                    _writer.GetAwaiter().GetResult();
+                }
+                catch (Exception)
+                {
+                    // Do nothing since we're in dispose method.
+                }
+
+                _writer.Dispose();
+
+                _innerStream.Dispose();
+                _cancellationTokenSource.Dispose();
+                _readBuffer.Dispose();
+                _writeBuffer.Dispose();
+            }
+
+            _disposed = true;
+            base.Dispose(disposing);
+        }
+
+        public override sealed async ValueTask DisposeAsync()
+        {
+            if (_disposed) return;
+
+            await DisposeAsyncCore();
+
+            _disposed = true;
+            Dispose(false);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            _cancellationTokenSource.Cancel();
+
+            try
+            {
+                await _writer;
+            }
+            catch (Exception)
+            {
+                // Do nothing since we're in dispose method.
+            }
+
+            _writer.Dispose();
+
+            await _innerStream.DisposeAsync();
+            _cancellationTokenSource.Dispose();
+            await _readBuffer.DisposeAsync();
+            await _writeBuffer.DisposeAsync();
         }
     }
 }
