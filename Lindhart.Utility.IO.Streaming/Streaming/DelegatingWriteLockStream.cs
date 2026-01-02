944dde6465fe1b33ae7617f63c64b1e373085fe3
Morten Hartlev Lindhart <MORTL@danskebank.dk>
2026-01-02 10:46:38 +0100

    Made the inverter obsolete.

--- Lindhart.Utility.IO.Streaming/Streaming/DelegatingWriteLockStream.cs
+++ Lindhart.Utility.IO.Streaming/Streaming/DelegatingWriteLockStream.cs
@@ -5,7 +5,7 @@ using System.Threading;
 
 namespace Lindhart.Utility.IO.Streaming
 {
-
+    [Obsolete("Use System.IO.Pipelines.Pipe instead. This is part of the .Net library and has better implementation.")]
     public sealed partial class StreamInverter
     {
         /// <summary>
